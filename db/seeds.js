7c462024bc60efa6e4069c6d561ac3be2a31fd6b
dav-cho <chodavid@gmail.com>
2021-05-13 11:02:25 -0400

    seed database

---
+++ db/seeds.js
@@ -0,0 +1,17 @@
+const mongoose = require('./connection');
+
+const Shoe = require('../models/shoe');
+const shoeData = require('./sneaker-db.json');
+
+const seedShoes = async () => {
+  try {
+    await Shoe.deleteMany({});
+    await Shoe.insertMany(shoeData.results);
+  } catch (err) {
+    console.log('~ err', err);
+  } finally {
+    mongoose.connection.close();
+  }
+};
+
+seedShoes();
