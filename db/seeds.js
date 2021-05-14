fc7f7baef26f3701d611e0d1938b2724b33d6a1d
c1894p <csp1894@gmail.com>
2021-05-14 13:48:58 -0400

    model updates and seeding

--- db/seeds.js
+++ db/seeds.js
@@ -1,32 +1,59 @@
-const mongoose = require('./connection');
+const mongoose = require("./connection");
 
-const shoeData = require('./seed-data/sneaker-db.json');
-const Shoe = require('../models/shoe.model');
+const shoeData = require("./seed-data/sneaker-db.json");
+const Shoe = require("../models/shoe.model");
 
-const User = require('../models/user.model');
-const userData = require('./seed-data/users-db.json');
+const User = require("../models/user.model");
+const userData = require("./seed-data/users-db.json");
 
 const seedShoes = async () => {
   try {
     await Shoe.deleteMany({});
     await Shoe.insertMany(shoeData.results);
   } catch (err) {
-    console.log('~ err', err);
+    console.log("~ err", err);
   } finally {
-    mongoose.connection.close();
+    await mongoose.connection.close();
   }
 };
 
 seedShoes();
+
+// const seedInventory = async () => {
+//   try {
+//     const random = Math.floor(Math.random() * 21);
+
+//     await Shoe.updateMany({}, {
+//       $set: {
+//         inventory: {
+//           "5": random,
+//           "6": random,
+//           "7": random,
+//           "8": random,
+//           "9": random,
+//           "10": random,
+//           "11": random,
+//           "12": random,
+//         }
+//       }
+//     })
+//   } catch (err) {
+//     console.log("~ err", err);
+//   } finally {
+//     await mongoose.connection.close();
+//   }
+// };
+
+// seedInventory();
 
 const seedUsers = async () => {
   try {
     await User.deleteMany({});
     await User.insertMany(userData.users);
   } catch (err) {
-    console.log('~ err', err);
+    console.log("~ err", err);
   } finally {
-    mongoose.connection.close();
+    await mongoose.connection.close();
   }
 };
 
--- models/user.model.js
+++ models/user.model.js
@@ -1,13 +1,19 @@
-const mongoose = require('../db/connection');
+const mongoose = require("../db/connection");
 
 const userSchema = new mongoose.Schema({
   username: String,
   password: String,
   email: String,
+  address: {
+    street:String,
+    city: String,
+    state: String,
+    zipcode: Number,
+  },
   favorites: [Object],
   cart: [Object],
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
